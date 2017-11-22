ee089eb9a3a4ffee4396366e7d93d98c252714cd
Regan Sarwas <regan_sarwas@nps.gov>
2017-11-21 16:29:49 -0900

    Adds better error checking

--- find-mosaic-data.py
+++ find-mosaic-data.py
@@ -21,9 +21,22 @@ def check_gdb(gdb, outputter=None):
     # I cant use a real temp file because to get a name, I must create it,
     # and arcpy.Export..() doesn't like the file existing even with arcpy.env.overwriteOutput = True
     temp_folder = tempfile.mkdtemp(prefix='mosaic_paths')
-    for item in arcpy.ListDatasets('*', 'Mosaic'):
-        print('{0},{1}'.format(gdb, item))
+    mosaics = arcpy.ListDatasets('*', 'Mosaic')
+    contents = 'JustMosaics'
+    if mosaics:
+        all_items = arcpy.ListDatasets('*')
+        if len(mosaics) != len(all_items):
+            contents = 'Mixed'
+    for item in mosaics:
         mosaic = os.path.join(gdb, item)
+        try:
+            ref = arcpy.Describe(mosaic).referenced
+        except arcpy.ExecuteError as ex:
+            print('{}, {}, {}'.format(gdb, item, ex))
+            continue
+        print('{0},{1},{2},{3}'.format(gdb, item, contents, ref))
+        if ref:
+            continue
         out_table = os.path.join(temp_folder, '{}.dbf'.format(item))
         # exports all paths and path types
         try:
@@ -31,9 +44,10 @@ def check_gdb(gdb, outputter=None):
         except arcpy.ExecuteError as ex:
             print('{}, {}, {}'.format(gdb, item, ex))
             continue
-        with arcpy.da.SearchCursor(out_table, "Path") as cursor:
+        with arcpy.da.SearchCursor(out_table, "Path,SourceID") as cursor:
             for row in cursor:
                 path = row[0]
+                sid = row[1]
                 folder, name = os.path.split(path)
                 name, ext = os.path.splitext(name)
                 size = -1
@@ -42,7 +56,7 @@ def check_gdb(gdb, outputter=None):
                 except os.error:
                     pass
                 if outputter is not None:
-                    outputter([gdb, item, folder, name, ext, size])
+                    outputter([gdb, item, folder, name, ext, sid, size])
                 else:
                     results.append([gdb, item, folder, name, ext, size])
         os.remove(out_table)  # need to make sure locks are gone before removing folder
