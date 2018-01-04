d9d711bfabcd29bde7475d31eff48eb6cb09d2fd
phoefflernps <paul_hoeffler@nps.gov>
2018-01-03 17:06:18 -0900

    Remove retired or renamed batch files

--- kill-robocopy.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-REM Batch file to kill robocopy processes
-REM Run with administrative privileges or taskkill will fail
-REM Absolute path for log file recommended, otherwise may be written to C:\Windows\System32 depending upon how batch file is executed
-REM Suggested application: schedule to run in the morning
-
-taskkill /f /im Robocopy.exe > kill-robocopy.log 2>&1
