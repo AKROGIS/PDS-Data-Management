c0fcf8bb5dbd20600d1e0005ae7a4ec5966532c7
phoefflernps <paul_hoeffler@nps.gov>
2018-01-09 19:27:26 -0900

    Consolidated scripts into folder for functions related to drive mappings

---
+++ x-mappings/make-remote-server-links.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Creates symbolic links for remote servers
+REM Need to run with administrative privileges
+mklink /D XDrive-AKRTestServer1 \\inpakro42491\GISData
+mklink /D XDrive-AKRTestServer2 \\inpakro38816\GISData2
+mklink /D XDrive-KEFJ \\inpkefjvfs\GISData
