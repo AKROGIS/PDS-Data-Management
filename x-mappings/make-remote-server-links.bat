6a78ac798158351682e209d70afecba81b847b39
phoefflernps <paul_hoeffler@nps.gov>
2018-01-10 16:04:41 -0900

    Added YUGA

--- x-mappings/make-remote-server-links.bat
+++ x-mappings/make-remote-server-links.bat
@@ -4,3 +4,4 @@ REM Need to run with administrative privileges
 mklink /D XDrive-AKRTestServer1 \\inpakro42491\GISData
 mklink /D XDrive-AKRTestServer2 \\inpakro38816\GISData2
 mklink /D XDrive-KEFJ \\inpkefjvfs\GISData
+mklink /D XDrive-YUGA \\inpyugavmgis\GISData
