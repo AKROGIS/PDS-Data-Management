3158561ae6a0a4d1aa8d6800a9c5bcc7fee94472
Regan Sarwas <regan_sarwas@nps.gov>
2018-11-29 21:15:42 -0900

    Add db capabilities to log processor

--- LogProcessor/process_robo_logs.py
+++ LogProcessor/process_robo_logs.py
@@ -1,7 +1,11 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-import os
-import time
+import datetime
+import glob
 import logging
+import os
+import re
+import sqlite3
+import time
 # import logging.config
 # import config_logger
 # logging.config.dictConfig(config_logger.config)
@@ -59,20 +63,39 @@ def process_summary_line(line, sentinal, errors):
 
 
 def process_error(line, errors, park):
-    if 'ERROR 53 (0x00000035)' in line:
-        #TODO: if we do not find 'ERROR: RETRY LIMIT EXCEEDED.' in a future line, then ignore
-        if 53 not in errors:
-            errors.append(53)
-            logger.error('Remote server %s unreachable', park)
+    if 'ERROR 2 (0x00000002)' in line:
+        if 2 not in errors:
+            errors.append(2)
+            logger.error('%s: The system cannot find the file specified.', park)
     elif 'ERROR 5 (0x00000005)' in line:
         #TODO: if we do not find 'ERROR: RETRY LIMIT EXCEEDED.' in a future line, then ignore
         if 5 not in errors:
             errors.append(5)
-            logger.error('%s: Access is denied', park)
+            logger.error('%s: Access is denied.', park)
+    elif 'ERROR 21 (0x00000015)' in line:
+        #TODO: if we do not find 'ERROR: RETRY LIMIT EXCEEDED.' in a future line, then ignore
+        if 21 not in errors:
+            errors.append(21)
+            logger.error('%s: The device is not ready.', park)
     elif 'ERROR 32 (0x00000020)' in line:
         if 32 not in errors:
             errors.append(32)
             logger.warning('%s: File is locked', park)
+    elif 'ERROR 53 (0x00000035)' in line:
+        #TODO: if we do not find 'ERROR: RETRY LIMIT EXCEEDED.' in a future line, then ignore
+        if 53 not in errors:
+            errors.append(53)
+            logger.error('Remote server %s unreachable', park)
+    elif 'ERROR 59 (0x0000003B)' in line:
+        if 59 not in errors:
+            errors.append(59)
+            logger.warning('%s: An unexpected network error occurred.', park)
+    elif 'ERROR 121 (0x00000079)' in line:
+        #TODO: if we do not find 'ERROR: RETRY LIMIT EXCEEDED.' in a future line, then ignore
+        #TODO: This is often found with Error 32, consolidate, and write file name to log
+        if 121 not in errors:
+            errors.append(121)
+            logger.warning('%s: The semaphore timeout period has expired.', park)
     else:
         logger.error('%s: Unexpected Error: %s', park, line)
         errors.append('Unknown ERROR')
@@ -107,18 +130,186 @@ def process_park(file_name):
         file_handle.close()
     return results
 
+def db_clear(db, drop=True):
+    try:
+        cursor = db.cursor()
+        if drop:
+            cursor.execute('DROP INDEX IF EXISTS logs_date_ix')
+            cursor.execute('DROP TABLE IF EXISTS logs')
+            cursor.execute('DROP TABLE IF EXISTS stats')
+        else:
+            cursor.execute('DELETE FROM logs')
+            cursor.execute('DELETE FROM stats')
+        db.commit()
+    except sqlite3.OperationalError:
+        pass
 
-def main():
+
+def db_write_stats(db, stats):
+    cursor = db.cursor()
+    cursor.executemany('''
+        INSERT INTO stats (log_id, stat, copied, extra, failed, mismatch, skipped, total)
+        VALUES (:log, :stat, :copied, :extra, :failed, :mismatch, :skipped, :total)
+    ''', stats)
+    db.commit()
+
+
+def db_write_log(db, log):
+    cursor = db.cursor()
+    cursor.execute('''
+        INSERT INTO logs (park, date, finished, errors)
+        VALUES (:name, :date, :finished, :errors)
+    ''', log)
+    log_id = cursor.lastrowid
+    db.commit()
+    return log_id
+
+
+def db_get_rows(db, sql):
+    cursor = db.cursor()
+    rows = cursor.execute(sql).fetchall()
+    return rows
+
+
+def db_create(db):
+    cursor = db.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS logs(
+            log_id INTEGER PRIMARY KEY,
+            park TEXT,
+            date TEXT,
+            finished INTEGER,
+            errors TEXT);
+    ''')
+    cursor.execute('''
+        CREATE INDEX IF NOT EXISTS logs_date_ix ON logs(date);
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS stats(
+            stat_id INTEGER PRIMARY KEY,
+            log_id INTEGER NOT NULL,
+            stat TEXT,
+            copied INTEGER,
+            extra INTEGER,
+            failed INTEGER,
+            mismatch INTEGER,
+            skipped INTEGER,
+            total INTEGER,
+            FOREIGN KEY(log_id) REFERENCES logs(artistid));
+    ''')
+    db.commit()
+
+
+def park(filename):
+    pattern = '([A-Z]{4})'
+    return re.search(pattern, filename).group()
+
+
+def get_files(dir,year,month,day):
+    template = '{0}-{1:02d}-{2:02d}*-update-x-drive.log'
+    pattern = os.path.join(dir, template.format(year, month, day))
+    #print(pattern)
+    files = glob.glob(pattern)
+    park_files = [(park(name), name) for name in files]
+    return park_files
+
+
+def main(db_name):
+    # TODO: if yesterdays logs are not found, then log an email error
     # import glob
     # filelist = glob.glob('data/Logs/*-update-x-drive.log')
-    filelist = ['data/Logs/2018-02-12_22-00-01-DENA-update-x-drive.log']
-    for filename in filelist:
-        print(filename)
-        print('='*len(filename))
-        res = process_park(filename)
-        print(res)
-        print()
+    # today = datetime.date.today()
+    # filelist = get_files('./LogProcessor/Logs', today.year, today.month, today.day-1)
+    # filelist = ['data/Logs/2018-02-12_22-00-01-DENA-update-x-drive.log']
+    filelist = glob.glob('./LogProcessor/Logs/2018-10-*-update-x-drive.log')
+    with sqlite3.connect(db_name) as conn:
+        for filename in filelist:
+            print(filename)
+            # TODO protect against Parsing errors
+            log = process_park(filename)
+            if not log:
+                print("EEK, Programming Error! The log object is empty")
+                #TODO email log
+                continue
+            for item in ['name', 'date', 'finished', 'errors']:
+                if item not in log:
+                    print("EEK, Programmingh Error! we got a bad log object " + item + " is missing")
+                    #TODO email log
+                    continue
+            log['errors'] = str(log['errors']) if log['errors'] else None
+            # TODO protect against DB errors
+            log_id = db_write_log(conn, log)
+            if not log_id:
+                print("EEK, unable to write log to database")
+                #TODO email log
+                continue
+            if log['finished']:
+                # We do not expect to get stats when the robo didn't finish
+                if 'stats' in log:
+                    stats = []
+                    for stat in ['dirs','files','bytes','times']:
+                        if stat not in log['stats']:
+                            print("EEK, Programming Error! We got a bad stats object, missing: " + stat)
+                            #TODO email log
+                            continue
+                        obj = log['stats'][stat]
+                        for item in ['copied', 'extra', 'failed', 'mismatch', 'skipped', 'total']:
+                            if item not in obj:
+                                print("EEK, Programming Error! we got a bad stats object " + stat + "/" + item + " is missing")
+                                #TODO email log
+                                continue
+                        obj['log'] = log_id
+                        obj['stat'] = stat
+                        stats.append(obj)
+                    # TODO protect against DB errors
+                    db_write_stats(conn, stats)
+                else:
+                    print("EEK!  Not stats for this file.")
+                    # Example in ./LogProcessor/Logs/2018-11-07_22-00-02-KLGO-update-x-drive.log
+                    # ./LogProcessor/Logs/2018-10-02_22-00-03-NOME-update-x-drive.log
+                    #TODO: log an email error
+
+
+def clean(db_name):
+    with sqlite3.connect(db_name) as conn:
+        db_clear(conn, drop=False)
+        db_create(conn)
+
+
+def db_testing(db_name):
+    with sqlite3.connect(db_name) as conn:
+        db_create(conn)
+        log_id = db_write_log(conn,
+            #{'name':'DENA', 'date':'2018-02-12', 'finished':True, 'errors':None}
+            {'name':'DENA', 'date':'2018-02-12', 'finished':True, 'errors':str(['e1','e2']), 'junk':5}
+        )
+        db_write_stats(conn,
+            [
+                {'log':log_id, 'stat': u'dirs', u'skipped': 3, u'extra': 2, u'mismatch': 0, u'failed': 0, u'copied': 0, u'total': 4711},
+                {'log':log_id, 'stat': u'files', u'skipped': 234433, u'extra': 0, u'mismatch': 0, u'failed': 0, u'copied': 0, u'total': 234433},
+                {'log':log_id, 'stat': u'bytes', u'skipped': 557141000000, u'extra': 0, u'mismatch': 0, u'failed': 0, u'copied': 0, u'total': 557141000000},
+                {'log':log_id, 'stat': u'times', u'skipped': 0, u'extra': 212, u'mismatch': 0, u'failed': 0, u'copied': 0, u'total': 212}
+            ]
+        )
+        sql = 'SELECT * FROM stats JOIN logs on stats.log_id = logs.log_id;'
+        print(db_get_rows(conn, sql))
+        db_clear(conn)
+        print(db_get_rows(conn, sql))
+
+
+def test_queries(db_name):
+    q1 = "select l.park, l.date, s.* from stats as s join logs as l on l.log_id = s.log_id where s.stat = 'dirs' and l.park = 'DENA' and l.errors is null order by l.date;"
+    q2 = "select date, count(*) from logs group by date order by date;"
+    q3 = "select park,date,errors from logs where errors is not null order by park,date;"
+    q4 = "select park,date,errors from logs where finished = 0 order by park,date;"
+    q5 = "select l.date, s.total, count(*), min(l.park), max(l.park) from stats as s join logs as l on l.log_id = s.log_id where s.stat = 'files' group by l.date, s.total;"
+    with sqlite3.connect(db_name) as conn:
+        for q in [q1,q2,q3,q4]:
+            print(db_get_rows(conn, q))
 
 
 if __name__ == '__main__':
-    main()
+    #db_testing(':memory:')
+    #clean('logs.db')
+    main('logs.db')
+    #print(process_park('./LogProcessor/Logs/2018-11-07_22-00-02-KLGO-update-x-drive.log'))
