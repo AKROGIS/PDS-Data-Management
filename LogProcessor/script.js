47fdffd10cc13b07d47a7e83bce3626bd875f427
Regan Sarwas <regan_sarwas@nps.gov>
2018-12-27 15:03:12 -0900

    Replace Snake Case with Camel Case

--- LogProcessor/script.js
+++ LogProcessor/script.js
@@ -1,9 +1,9 @@
-const data_server = '//inpakrovmais:8080'
-//const data_server = '//localhost:8080'
 // NOTE: all dates in this code are ISO formated date strings (YYYY-MM-DD) in local time.
 //      in the few cases where a javascript date object is needed it is called dateObj
 
-const data_server = '//localhost:8080'
+// The name of the server with the data for this page
+const dataServer = '//inpakrovmais:8080'
+// const dataServer = '//localhost:8080'
 
 // Return bytes as human readable quantity
 // Credit: https://stackoverflow.com/a/14919494
@@ -145,43 +145,43 @@ function getJSON (url, callback, errorback) {
 }
 
 // Success callback for adding summary data to the web page
-function post_summary (data) {
+function postSummary (data) {
   document.getElementById('summary_wait').hidden = true
   if (Object.keys(data).length === 0 && data.constructor === Object) {
     document.getElementById('summary_fail').hidden = false
     return
   }
   const date = data['summary_date']
-  const count_starts = data['count_start']
-  const count_unfinished = data['count_unfinished']
-  const count_errors = data['count_with_errors']
-  const has_changes = data['has_changes']
-  const issues = count_errors > 0 || count_unfinished > 0
+  const countStarts = data['count_start']
+  const countUnfinished = data['countUnfinished']
+  const countErrors = data['count_with_errors']
+  const hasChanges = data['hasChanges']
+  const issues = countErrors > 0 || countUnfinished > 0
 
-  const count_ele = document.getElementById('count_total_parks')
-  if (count_starts == 0) {
-    count_ele.textContent = 'no parks'
-  } else if (count_starts > 1) {
-    count_ele.textContent = count_starts + ' parks'
+  const countEle = document.getElementById('count_total_parks')
+  if (countStarts == 0) {
+    countEle.textContent = 'no parks'
+  } else if (countStarts > 1) {
+    countEle.textContent = countStarts + ' parks'
   }
   // case of "1 Park" is the default in the html
 
-  if (count_unfinished > 1) {
+  if (countUnfinished > 1) {
     document.getElementById('summary_incomplete_count').textContent =
-      count_unfinished + ' parks'
+      countUnfinished + ' parks'
   }
-  document.getElementById('summary_incomplete').hidden = count_unfinished == 0
+  document.getElementById('summary_incomplete').hidden = countUnfinished == 0
 
-  if (count_errors > 1) {
+  if (countErrors > 1) {
     document.getElementById('summary_errors_count').textContent =
-      count_errors + ' parks'
+      countErrors + ' parks'
   }
-  document.getElementById('summary_errors').hidden = count_errors == 0
+  document.getElementById('summary_errors').hidden = countErrors == 0
 
   if (issues) {
     document.getElementById('summary_issues').hidden = false
     document.getElementById('summary_no_issues').hidden = true
-    if (count_errors == 0) {
+    if (countErrors == 0) {
       document
         .getElementById('summary_card')
         .classList.replace('nominal', 'warning')
@@ -195,7 +195,7 @@ function post_summary (data) {
     document.getElementById('summary_no_issues').hidden = false
   }
 
-  if (has_changes) {
+  if (hasChanges) {
     document.getElementById('summary_changes').hidden = false
     document.getElementById('summary_no_changes').hidden = true
     document.getElementById('changelog_link').href =
@@ -208,67 +208,67 @@ function post_summary (data) {
 }
 
 // Success callback for adding park details to the web page
-function post_park_details (data) {
+function postParkDetails (data) {
   if (data.length === 1) {
     document.getElementById('park_wait').hidden = true
     document.getElementById('park_fail').hidden = false
     return
   }
-  // ["park","date","finished","count_errors","files_copied","files_removed","files_scanned","time_copying","time_scanning","bytes_copied"]
+  // ["park","date","finished","countErrors","files_copied","filesRemoved","filesScanned","timeCopying","timeScanning","bytesCopied"]
   // Ignore the first row (header), assume there are no more then 20 parks
   let html = ''
   data.slice(1, 20).forEach(row => {
     const park = row[0]
     const date = row[1]
-    const bytes_copied = row[9]
-    const size_copied = humanFileSize(bytes_copied, true)
-    const time_copying = row[7]
-    const copy_speed = round(bytes_copied / time_copying / 1000.0, 1)
-    let copy_text =
-      time_copying == 0
+    const bytesCopied = row[9]
+    const sizeCopied = humanFileSize(bytesCopied, true)
+    const timeCopying = row[7]
+    const copySpeed = round(bytesCopied / timeCopying / 1000.0, 1)
+    let copyText =
+      timeCopying == 0
         ? 'Nothing copied.'
-        : `${size_copied} in ${time_copying} seconds (${copy_speed} kB/sec)`
-    copy_text = time_copying == null ? 'Unknown' : copy_text
-    const files_scanned = row[6]
-    const time_scanning = row[8]
-    const scan_speed = round(files_scanned / time_scanning, 1)
-    const files_removed = row[5]
+        : `${sizeCopied} in ${timeCopying} seconds (${copySpeed} kB/sec)`
+    copyText = timeCopying == null ? 'Unknown' : copyText
+    const filesScanned = row[6]
+    const timeScanning = row[8]
+    const scanSpeed = round(filesScanned / timeScanning, 1)
+    const filesRemoved = row[5]
     const finished = row[2]
-    const count_errors = row[3]
+    const countErrors = row[3]
     const status =
-      count_errors == 0 ? (finished == 1 ? 'nominal' : 'warning') : 'error'
-    const error_str = count_errors == 0 ? '' : `${count_errors} Errors.`
-    const finish_str =
+      countErrors == 0 ? (finished == 1 ? 'nominal' : 'warning') : 'error'
+    const errorStr = countErrors == 0 ? '' : `${countErrors} Errors.`
+    const finishStr =
       finished == 1 ? '' : 'Robocopy did not finish (no timing data).'
     let issues = 'No Issues.'
-    if (error_str == '' && finish_str != '') {
-      issues = finish_str
-    } else if (error_str != '' && finish_str == '') {
-      issues = error_str
-    } else if (error_str != '' && finish_str != '') {
-      issues = error_str + ' ' + finish_str
+    if (errorStr == '' && finishStr != '') {
+      issues = finishStr
+    } else if (errorStr != '' && finishStr == '') {
+      issues = errorStr
+    } else if (errorStr != '' && finishStr != '') {
+      issues = errorStr + ' ' + finishStr
     }
-    const scan_text =
-      files_scanned == null
+    const scanText =
+      filesScanned == null
         ? 'Unknown'
-        : `${files_scanned} files in ${time_scanning} seconds (${scan_speed} files/sec)`
-    const removed_text =
-      files_removed == null ? 'Unknown' : `${files_removed} files`
-    const card_str = `
+        : `${filesScanned} files in ${timeScanning} seconds (${scanSpeed} files/sec)`
+    const removedText =
+      filesRemoved == null ? 'Unknown' : `${filesRemoved} files`
+    const cardStr = `
       <div class='card ${status} inline'>
         <h3>${park}</h3>
         <dt>Copied</dt>
-        <dd>${copy_text}</dd>
+        <dd>${copyText}</dd>
         <dt>Scanned</dt>
-        <dd>${scan_text}</dd>
+        <dd>${scanText}</dd>
         <dt>Removed</dt>
-        <dd>${removed_text}</dd>
+        <dd>${removedText}</dd>
         <dt>Issues</dt>
         <dd>${issues}</dd>
-        <a href='${data_server}/logfile?park=${park}&date=${date}'>Log file</a>
+        <a href='${dataServer}/logfile?park=${park}&date=${date}'>Log file</a>
       </div>
     `
-    html += card_str
+    html += cardStr
   })
   document.getElementById('park_cards').innerHTML = html
   document.getElementById('park_wait').hidden = true
@@ -276,7 +276,7 @@ function post_park_details (data) {
 }
 
 // Error callback for adding summary error to the web page
-function summary_failed (message) {
+function summaryFailed (message) {
   const ele = document.getElementById('summary_fail')
   if (message == 'Service Unavailable') {
     const message2 = 'Check to make sure the python service is running.'
@@ -289,7 +289,7 @@ function summary_failed (message) {
 }
 
 // Error callback for adding park details error to the web page
-function parks_failed (message) {
+function parksFailed (message) {
   const ele = document.getElementById('park_fail')
   ele.textContent = message
   ele.hidden = false
@@ -297,25 +297,25 @@ function parks_failed (message) {
 }
 
 // Update the state of the date text and the next/previous date buttons
-function fix_date_button_state (date) {
-  const previous_button = document.getElementById('previous_date')
-  const next_button = document.getElementById('next_date')
+function fixDateButtonState (date) {
+  const previousButton = document.getElementById('previous_date')
+  const nextButton = document.getElementById('next_date')
 
   // Use a javascript date object because it makes date math much easier
   //   getDate and setDate get/set the day of the month
   //   setDate will roll up/down a month/year as needed if the new day is out of range
   let dateObj = dateFromIso(date)
   dateObj.setDate(dateObj.getDate() + 1) // Add one day to the date
-  next_button.dataset.destination = isoDateFormat(dateObj)
+  nextButton.dataset.destination = isoDateFormat(dateObj)
   dateObj = dateFromIso(date)
   dateObj.setDate(dateObj.getDate() - 1) // Subtract one day to the date
-  previous_button.dataset.destination = isoDateFormat(dateObj)
+  previousButton.dataset.destination = isoDateFormat(dateObj)
 
-  previous_button.hidden = date <= previous_button.dataset.limit
-  next_button.hidden = next_button.dataset.limit <= date
+  previousButton.hidden = date <= previousButton.dataset.limit
+  nextButton.hidden = nextButton.dataset.limit <= date
 }
 
-function plot_2bars (x, l1, y1, l2, y2, title) {
+function plot2bars (x, l1, y1, l2, y2, title) {
   const trace1 = {
     x: x,
     y: y1,
@@ -336,7 +336,7 @@ function plot_2bars (x, l1, y1, l2, y2, title) {
   Plotly.newPlot('graph_div', [trace1, trace2], layout)
 }
 
-function plot_2lines (x, l1, y1, l2, y2, title) {
+function plot2lines (x, l1, y1, l2, y2, title) {
   const trace1 = {
     x: x,
     y: y1,
@@ -357,7 +357,7 @@ function plot_2lines (x, l1, y1, l2, y2, title) {
   Plotly.newPlot('graph_div', [trace1, trace2], layout)
 }
 
-function plot_5lines (x, l1, y1, l2, y2, l3, y3, l4, y4, l5, y5, title) {
+function plot5lines (x, l1, y1, l2, y2, l3, y3, l4, y4, l5, y5, title) {
   const trace1 = {
     x: x,
     y: y1,
@@ -407,17 +407,17 @@ function unpack (rows, key) {
 
 function plot1 (data) {
   if (data.length < 2) {
-    get_plot_data_fail('No plot data for this date.')
+    getPlotDataFail('No plot data for this date.')
     return
   }
   document.getElementById('graph_wait').hidden = true
   document.getElementById('graph_div').hidden = false
-  plot_2bars(
+  plot2bars(
     unpack(data, 0), // park
     'Copy Speed (kB/s)',
-    unpack(data, 2), // copy_speed
+    unpack(data, 2), // copySpeed
     'Scan Speed (files/s)',
-    unpack(data, 1), // scan_speed
+    unpack(data, 1), // scanSpeed
     'Park Speed Comparison (single night)'
   )
 }
@@ -425,7 +425,7 @@ function plot1 (data) {
 function plot2 (data) {
   document.getElementById('graph_wait').hidden = true
   document.getElementById('graph_div').hidden = false
-  plot_2bars(
+  plot2bars(
     unpack(data, 0), // park
     'Scan Speed (files/s)',
     unpack(data, 1), // avg scan speed
@@ -438,7 +438,7 @@ function plot2 (data) {
 function plot3 (data) {
   document.getElementById('graph_wait').hidden = true
   document.getElementById('graph_div').hidden = false
-  plot_2bars(
+  plot2bars(
     unpack(data, 0), // park
     'Copy Speed (kB/s)',
     unpack(data, 1), // avg copy speed
@@ -453,7 +453,7 @@ function plot4 (data) {
   document.getElementById('graph_div').hidden = false
   const park = data[0][0]
   const title = 'Historic Speeds for ' + park
-  plot_2lines(
+  plot2lines(
     // data 0 has the park name
     unpack(data, 1),
     'Scan Speed (files/s)',
@@ -469,7 +469,7 @@ function plot4a (data) {
   document.getElementById('graph_div').hidden = false
   const park = data[0][0]
   const title = 'Historic speeds for ' + park
-  plot_5lines(
+  plot5lines(
     // data 0 has the park name
     unpack(data, 1),
     'Scan Speed (files/s)',
@@ -488,7 +488,7 @@ function plot4a (data) {
   document.getElementById('graph_div').hidden = false
 }
 
-function get_plot_data_fail (err) {
+function getPlotDataFail (err) {
   const ele = document.getElementById('graph_fail')
   ele.hidden = false
   if (err) {
@@ -501,21 +501,21 @@ function get_plot_data_fail (err) {
 // DOM Events
 // ===========
 
-function next_date () {
-  const new_date = document.getElementById('next_date').dataset.destination
-  const url = UpdateQueryString('date', new_date)
+function nextDate () {
+  const newDate = document.getElementById('next_date').dataset.destination
+  const url = UpdateQueryString('date', newDate)
   window.history.pushState({}, '', url)
-  setup_page(new_date)
+  setupPage(newDate)
 }
 
-function previous_date () {
-  const new_date = document.getElementById('previous_date').dataset.destination
-  const url = UpdateQueryString('date', new_date)
+function previousDate () {
+  const newDate = document.getElementById('previous_date').dataset.destination
+  const url = UpdateQueryString('date', newDate)
   window.history.pushState({}, '', url)
-  setup_page(new_date)
+  setupPage(newDate)
 }
 
-function prep_for_new_graph () {
+function prepForNewGraph () {
   const graph = document.getElementById('graph_div')
   graph.hidden = true
   document.getElementById('graph_fail').hidden = true
@@ -525,58 +525,58 @@ function prep_for_new_graph () {
   }
 }
 
-function plot_parks1 () {
-  prep_for_new_graph()
+function plotParks1 () {
+  prepForNewGraph()
   const date = document.getElementById('page_date').textContent
-  const url = data_server + '/plot1?date=' + date
-  getJSON(url, plot1, get_plot_data_fail)
+  const url = dataServer + '/plot1?date=' + date
+  getJSON(url, plot1, getPlotDataFail)
 }
 
-function plot_parks2 () {
-  prep_for_new_graph()
+function plotParks2 () {
+  prepForNewGraph()
   document.getElementById('graph_fail').hidden = true
-  const url = data_server + '/scanavg?date=2018-09-01'
-  getJSON(url, plot2, get_plot_data_fail)
+  const url = dataServer + '/scanavg?date=2018-09-01'
+  getJSON(url, plot2, getPlotDataFail)
 }
 
-function plot_parks3 () {
-  prep_for_new_graph()
+function plotParks3 () {
+  prepForNewGraph()
   document.getElementById('graph_fail').hidden = true
-  const url = data_server + '/copyavg?date=2018-09-01'
-  getJSON(url, plot3, get_plot_data_fail)
+  const url = dataServer + '/copyavg?date=2018-09-01'
+  getJSON(url, plot3, getPlotDataFail)
 }
 
-function plot_parks4 () {
-  prep_for_new_graph()
+function plotParks4 () {
+  prepForNewGraph()
   document.getElementById('graph_fail').hidden = true
-  const url = data_server + '/speed?park=YUGA&start=2018-07-01&end=2018-11-01'
-  getJSON(url, plot4, get_plot_data_fail)
+  const url = dataServer + '/speed?park=YUGA&start=2018-07-01&end=2018-11-01'
+  getJSON(url, plot4, getPlotDataFail)
 }
 
 // Get data from the services and update the page
-function setup_page (date) {
+function setupPage (date) {
   document.getElementById('page_date').textContent = date
-  fix_date_button_state(date)
+  fixDateButtonState(date)
   const query = '?date=' + date
   document.getElementById('summary_wait').hidden = false
   document.getElementById('summary_card').hidden = true
   document.getElementById('summary_fail').hidden = true
-  getJSON(data_server + '/summary' + query, post_summary, summary_failed)
+  getJSON(dataServer + '/summary' + query, postSummary, summaryFailed)
   document.getElementById('park_wait').hidden = false
   document.getElementById('park_cards').hidden = true
   document.getElementById('park_fail').hidden = true
-  getJSON(data_server + '/parks' + query, post_park_details, parks_failed)
+  getJSON(dataServer + '/parks' + query, postParkDetails, parksFailed)
 }
 
 // Get data from the services and update the page
-function setup_site () {
-  const last_night = getYesterday()
-  const first_night = '2018-01-22'
-  document.getElementById('previous_date').dataset.limit = first_night
-  document.getElementById('next_date').dataset.limit = last_night
+function setupSite () {
+  const lastNight = getYesterday()
+  const firstNight = '2018-01-22'
+  document.getElementById('previous_date').dataset.limit = firstNight
+  document.getElementById('next_date').dataset.limit = lastNight
   const params = new URLSearchParams(document.location.search.substring(1))
-  const date = validateDate(params.get('date'), first_night, last_night)
-  setup_page(date)
+  const date = validateDate(params.get('date'), firstNight, lastNight)
+  setupPage(date)
 }
 
-setup_site()
+setupSite()
