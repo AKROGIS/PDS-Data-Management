ab034895102827f338f9f5a2f491c74fcf074bb2
Regan Sarwas <regan_sarwas@nps.gov>
2019-05-09 14:57:32 -0800

    Add a simple command line verifier

---
+++ MapFixer/verifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MapFixer;
+
+namespace verifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataPath = args.Length > 0 ? args[0] : @"X:\GIS\ThemeMgr\DataMoves.csv";
+            Console.WriteLine($"Checking file: {dataPath}");
+            var _ = new Moves(dataPath,'|',true);
+        }
+    }
+}
