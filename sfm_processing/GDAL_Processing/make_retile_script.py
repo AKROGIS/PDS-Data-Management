d53172bdd4a2480df1a791797e698e768c429725
Regan Sarwas <regan_sarwas@nps.gov>
2020-12-23 12:30:06 -0900

    Fix for Issue #15

--- sfm_processing/GDAL_Processing/make_retile_script.py
+++ sfm_processing/GDAL_Processing/make_retile_script.py
@@ -1,3 +1,5 @@
+import sys
+
 # This script will create a GDAL commands to retile (sub-divide) images
 # into smaller chunks. The output can be saved into a batch file for execution
 # in the directory containing the source images.  commands assumes there is a
@@ -14,12 +16,21 @@ xsize = 5000
 ysize = 5000
 
 # The base name of the source images.
-# If there is one source image, then the filename should be {basename}.{ext}
-# If the image name is in the form {basename}-{r}-{c}.{ext} then set the
-# in-columns and in-rows settings to the range of r and c.
+# If the source image name is in the form {basename}-{c}-{r}.{ext}
+#   set in_columns and in_rows to the range of c and r respectively.
+#   set add_numbers = True
+#   NOTE: if in_columns or in_rows are > 1 then add_numbers is ignored (and assumed True)
+# If there is one source image with the format {basename}.{ext}
+#   set both in_columns and in-rows to 1.
+#   set add_numbers = False
+# If the column or row numbers in the source image are padded with leading zeros, i.e 00, 01, .. 99 
+#   set pad_column_with_leading_zeros and pad_row_with_leading_zeros to True or False appropriately
 base_name = "2018_BELA_ATV_South_RGB"
 in_columns = 17
 in_rows = 16
+add_numbers = True
+pad_column_with_leading_zeros = False
+pad_row_with_leading_zeros = False
 
 # Number of new tiles per source image
 # Adjust n_cols and n_rows, so that (n-1 * size) < source_size < (n * size)
@@ -34,17 +45,26 @@ cmd_base = "gdal_translate -of GTIFF {7} -srcwin {0:5} {1:5} {2:5} {3:5} -CO COM
 
 # End of configuration options.
 
+if in_columns < 1 or in_rows < 1:
+    print("Error: Both in_columns and in_rows must be greater than zero.")
+    sys.exit()
+if n_cols < 1 or n_rows < 1:
+    print("Error: Both n_cols and n_rows must be greater than zero.")
+    sys.exit()
+if n_cols < 2 and n_rows < 2:
+    print("Error: One of n_cols or n_rows must be greater than one.")
+    sys.exit()
 old_name = base_name
-cformat = "-{1}"
-rformat = "-{2}"
-c1format = "-{1}" if in_columns <= 10 else "-{1:02}"
-r1format = "-{2}" if in_rows <= 10 else "-{2:02}"
-c2format = "-{1}" if in_columns * n_cols <= 10 else "-{1:02}"
-r2format = "-{2}" if in_rows * n_rows <= 10 else "-{2:02}"
+cformat_in = "-{1:02}" if pad_column_with_leading_zeros else "-{1}"
+rformat_in = "-{2:02}" if pad_row_with_leading_zeros else "-{2}"
+cformat_out = "-{1}" if in_columns * n_cols <= 10 else "-{1:02}"
+rformat_out = "-{2}" if in_rows * n_rows <= 10 else "-{2:02}"
+if in_columns > 1 or in_rows > 1:
+    add_numbers = True
 for c in range(in_columns):
     for r in range(in_rows):
-        if in_columns > 1 or in_rows > 1:
-            old_name = ("{0}" + cformat + rformat).format(base_name,c, r)
+        if add_numbers:
+            old_name = ("{0}" + cformat_in + rformat_in).format(base_name,c, r)
         new_base_name = "new\\" + base_name
         start_cell_x = n_cols * c
         start_cell_y = n_rows * r
@@ -54,7 +74,7 @@ for c in range(in_columns):
             for j in range(n_rows):
                 yname = start_cell_y + j
 
-                new_name = ("{0}" + c2format + r2format).format(new_base_name, xname, yname)
+                new_name = ("{0}" + cformat_out + rformat_out).format(new_base_name, xname, yname)
                 yoff = ysize*j
                 cmd = cmd_base.format(xoff, yoff, xsize, ysize, old_name, new_name, predictor, extra)
                 print cmd
