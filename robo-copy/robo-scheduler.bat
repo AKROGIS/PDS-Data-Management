3f3f6b427c9af8e5d9f051abcb834bb59ce7a1f6
Regan Sarwas <regan_sarwas@nps.gov>
2021-01-04 17:53:38 -0900

    Update robo-scheduler.bat

--- robo-copy/robo-scheduler.bat
+++ robo-copy/robo-scheduler.bat
@@ -7,12 +7,25 @@ REM Usage: E:\XDrive\UpdateTools\robo-remote-server.bat REMOTE-UNC(destination) 
 REM Standard example: E:\XDrive\UpdateTools\robo-remote-server.bat c:\test c:\logs TEST "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:1800-0600 /PF"
 
 REM For increased logging/output remove /NDL and/or /NFL; add in to reduce log size
+REM Robo process kicks off at 6pm (18:00), and is killed (if still running) at 6:59
 REM To remove hours restriction remove /RH:1800-0600 /PF
 
 REM Starts all processes without /wait switch concurrently - can use /wait to sequence portions of the robocopy calls, e.g. include a /wait switch after each fifth start command
-REM start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\Xdrive\RemoteServers\XDrive-AKRTestServer1 %logpath% AKRTest1 "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ"
+REM start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\Xdrive\RemoteServers\XDrive-AKRTestServer1 %logpath% AKRTest1 "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /XD Local"
 REM start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\Xdrive\RemoteServers\XDrive-AKRTestServer2 %logpath% AKRTest2 "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ"
-start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\XDrive\RemoteServers\XDrive-KEFJ %logpath% KEFJ "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:1800-0600 /PF"
+start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\XDrive\RemoteServers\XDrive-KEFJ %logpath% KEFJ "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:2200-0600 /PF"
+start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\XDrive\RemoteServers\XDrive-YUGA %logpath% YUGA "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:2200-0600 /PF"
+start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\XDrive\RemoteServers\XDrive-WRST %logpath% WRST "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:2000-0600 /PF /XD GIS_WRST"
+REM start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\XDrive\RemoteServers\XDrive-KENN %logpath% KENN "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:1800-0600 /PF /XD GIS_WRST"
+start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\XDrive\RemoteServers\XDrive-DENA %logpath% DENA "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:2200-0600 /PF /XD Local Research_Files"
+start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\XDrive\RemoteServers\XDrive-LACL %logpath% LACL "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:1800-0600 /PF"
+start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\XDrive\RemoteServers\XDrive-KATM %logpath% KATM "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:1800-0600 /PF"
+start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\XDrive\RemoteServers\XDrive-SEAN %logpath% SEAN "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:2200-0600 /PF"
+start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\XDrive\RemoteServers\XDrive-SITK %logpath% SITK "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:2200-0600 /PF"
+start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\XDrive\RemoteServers\XDrive-NOME %logpath% NOME "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:2000-0600 /PF"
+start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\XDrive\RemoteServers\XDrive-KOTZ %logpath% KOTZ "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:2000-0600 /PF"
+start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\XDrive\RemoteServers\XDrive-KLGO %logpath% KLGO "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:2200-0600 /PF /XD"
+start /min CMD /c E:\XDrive\UpdateTools\robo-remote-server.bat E:\XDrive\RemoteServers\XDrive-GLBA %logpath% GLBA "/R:5 /W:5 /Z /MIR /NDL /NP /NS /NC /XJ /RH:2000-0600 /PF /XD GLBA_Local __DFSR_DIAGNOSTICS_TEST_FOLDER__ DfsrPrivate"
 
 REM Robocopy switches
 REM /R:5 - retry a file five times
