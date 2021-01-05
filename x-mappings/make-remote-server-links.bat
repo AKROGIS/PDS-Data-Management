6edf6847aa5b84ee35bd4b045d1fb2c5b6c90eb7
Regan Sarwas <regan_sarwas@nps.gov>
2021-01-04 17:48:04 -0900

    Update the mklink scripts

--- x-mappings/make-remote-server-links.bat
+++ x-mappings/make-remote-server-links.bat
@@ -1,7 +1,16 @@
 @echo off
 REM Creates symbolic links for remote servers
 REM Need to run with administrative privileges
-mklink /D XDrive-AKRTestServer1 \\inpakro42491\GISData
-mklink /D XDrive-AKRTestServer2 \\inpakro38816\GISData2
 mklink /D XDrive-KEFJ \\inpkefjvfs\GISData
 mklink /D XDrive-YUGA \\inpyugavmgis\GISData
+mklink /D XDrive-DENA \\inpdenagis\GIS_Data
+mklink /D XDrive-WRST \\INPWRSTvFS01\GIS_Data
+mklink /D XDrive-LACL \\INPLACLPAVFS\GIS
+mklink /D XDrive-SEAN \\INPGLBAFS03\GISData
+mklink /D XDrive-KATM \\inpkatmvfs\GIS
+mklink /D XDrive-SITK \\INPSITKvFS\SITK\GISData
+mklink /D XDrive-NOME \\INPWEARNOMEvFS\GIS
+mklink /D XDrive-KOTZ \\INPWEARKOTZvFS\GIS
+mklink /D XDrive-GLBA \\INPGLBAFSPri\GIS
+mklink /D XDrive-KENN \\inpwrstkennms\gis_data
+mklink /D XDrive-KLGO \\inpklgogis01\akro_gis
