f4d7571d5f32ca709b5c821c1e67d6e0d9d6c8a6
Regan Sarwas <Regan_Sarwas@nps.gov>
2021-01-23 15:08:47 -0900

    lint make_retile_script.py

--- sfm_processing/GDAL_Processing/make_retile_script.py
+++ sfm_processing/GDAL_Processing/make_retile_script.py
@@ -21,9 +21,12 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import sys
 
-# CONFIGURATION CONSTANTS
-class Config(object):
-    # Output Size  (10000 is good for 32bit 1band DEMs, and 5000 is good for 16bit-3band orthos)
+
+class Config:
+    """Configuration Constants; edit as necessary for each execution."""
+
+    # Output Size  (10000 is good for 32bit 1band DEM,
+    #   and 5000 is good for 16bit-3band orthos)
     # This keeps the output files in the 100 to 200 MB range
     x_size = 5000
     y_size = 5000
@@ -32,12 +35,14 @@ class Config(object):
     # If the source image name is in the form {basename}-{c}-{r}.{ext}
     #   set in_columns and in_rows to the range of c and r respectively.
     #   set add_numbers = True
-    #   NOTE: if in_columns or in_rows are > 1 then add_numbers is ignored (and assumed True)
+    #   NOTE: if in_columns or in_rows are > 1 then add_numbers is ignored
+    #   (and assumed True)
     # If there is one source image with the format {basename}.{ext}
     #   set both in_columns and in-rows to 1.
     #   set add_numbers = False
-    # If the column or row numbers in the source image are padded with leading zeros, i.e 00, 01, .. 99
-    #   set pad_column_with_leading_zeros and pad_row_with_leading_zeros to True or False appropriately
+    # If the column or row numbers in the source image are padded with leading
+    #   zeros, i.e 00, 01, .. 99 set pad_column_with_leading_zeros and
+    #   pad_row_with_leading_zeros to True or False appropriately
     base_name = "2018_BELA_ATV_South_RGB"
     in_columns = 17
     in_rows = 16
@@ -54,7 +59,10 @@ class Config(object):
     # GDAL command settings
     predictor = 2  # 2 for Ortho, 3 for DEM
     extra = ""  # for adding no-data or band removal  -a_nodata none  -b 1 -b 2 -b 3
-    cmd_base = "gdal_translate -of GTIFF {7} -srcwin {0:5} {1:5} {2:5} {3:5} -CO COMPRESS=DEFLATE -co PREDICTOR={6} -CO TILED=YES {4}.tif {5}.tif"
+    cmd_base = (
+        "gdal_translate -of GTIFF {7} -srcwin {0:5} {1:5} {2:5} {3:5} "
+        "-CO COMPRESS=DEFLATE -co PREDICTOR={6} -CO TILED=YES {4}.tif {5}.tif"
+    )
 
 
 def print_commands():
@@ -70,35 +78,35 @@ def print_commands():
         print("Error: One of n_cols or n_rows must be greater than one.")
         sys.exit()
     old_name = Config.base_name
-    cformat_in = "-{1:02}" if Config.pad_column_with_leading_zeros else "-{1}"
-    rformat_in = "-{2:02}" if Config.pad_row_with_leading_zeros else "-{2}"
-    cformat_out = "-{1}" if Config.in_columns * Config.n_cols <= 10 else "-{1:02}"
-    rformat_out = "-{2}" if Config.in_rows * Config.n_rows <= 10 else "-{2:02}"
+    c_format_in = "-{1:02}" if Config.pad_column_with_leading_zeros else "-{1}"
+    r_format_in = "-{2:02}" if Config.pad_row_with_leading_zeros else "-{2}"
+    c_format_out = "-{1}" if Config.in_columns * Config.n_cols <= 10 else "-{1:02}"
+    r_format_out = "-{2}" if Config.in_rows * Config.n_rows <= 10 else "-{2:02}"
     add_numbers = Config.add_numbers
     if Config.in_columns > 1 or Config.in_rows > 1:
         add_numbers = True
-    for c in range(Config.in_columns):
-        for r in range(Config.in_rows):
+    for col in range(Config.in_columns):
+        for row in range(Config.in_rows):
             if add_numbers:
-                old_name = ("{0}" + cformat_in + rformat_in).format(
-                    Config.base_name, c, r
+                old_name = ("{0}" + c_format_in + r_format_in).format(
+                    Config.base_name, col, row
                 )
             new_base_name = "new\\" + Config.base_name
-            start_cell_x = Config.n_cols * c
-            start_cell_y = Config.n_rows * r
+            start_cell_x = Config.n_cols * col
+            start_cell_y = Config.n_rows * row
             for i in range(Config.n_cols):
-                xoff = Config.x_size * i
-                xname = start_cell_x + i
+                x_off = Config.x_size * i
+                x_name = start_cell_x + i
                 for j in range(Config.n_rows):
-                    yname = start_cell_y + j
+                    y_name = start_cell_y + j
 
-                    new_name = ("{0}" + cformat_out + rformat_out).format(
-                        new_base_name, xname, yname
+                    new_name = ("{0}" + c_format_out + r_format_out).format(
+                        new_base_name, x_name, y_name
                     )
-                    yoff = Config.y_size * j
+                    y_off = Config.y_size * j
                     cmd = Config.cmd_base.format(
-                        xoff,
-                        yoff,
+                        x_off,
+                        y_off,
                         Config.x_size,
                         Config.y_size,
                         old_name,
@@ -108,11 +116,18 @@ def print_commands():
                     )
                     print(cmd)
                     if Config.predictor == 2:
-                        addo_cmd = "gdaladdo --config COMPRESS_OVERVIEW JPEG --config PHOTOMETRIC_OVERVIEW YCBCR --config INTERLEAVE_OVERVIEW PIXEL -r average {0}.tif"
-                        print(addo_cmd.format(new_name))
+                        add_cmd = (
+                            "gdaladdo --config COMPRESS_OVERVIEW JPEG "
+                            "--config PHOTOMETRIC_OVERVIEW YCBCR "
+                            "--config INTERLEAVE_OVERVIEW PIXEL -r average {0}.tif"
+                        )
+                        print(add_cmd.format(new_name))
                     if Config.predictor == 3:
-                        addo_cmd = "gdaladdo --config COMPRESS_OVERVIEW JPEG --config INTERLEAVE_OVERVIEW PIXEL -r average {0}.tif"
-                        print(addo_cmd.format(new_name))
+                        add_cmd = (
+                            "gdaladdo --config COMPRESS_OVERVIEW JPEG "
+                            "--config INTERLEAVE_OVERVIEW PIXEL -r average {0}.tif"
+                        )
+                        print(add_cmd.format(new_name))
                     print("gdalinfo -stats -hist {0}.tif".format(new_name))
 
 
