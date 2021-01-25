4be19bb03dcfaa8936501db85b7936c570f38a93
Regan Sarwas <Regan_Sarwas@nps.gov>
2021-01-24 17:49:01 -0900

    Save the test configuration as the default

--- remote-mover/config_file.py
+++ remote-mover/config_file.py
@@ -13,11 +13,11 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 # Path to the moves database.
 # https://github.com/AKROGIS/MapFixer for the definition of the moves database.
-MOVES_DB = r"X:\GIS\ThemeMgr\DataMoves.csv"
+# MOVES_DB = r"X:\GIS\ThemeMgr\DataMoves.csv"
 # Testing:
-# MOVES_DB = "test/moves.csv"
+MOVES_DB = "test/moves.csv"
 
 # Remote server X drive path: UNC path or symbolic link to location where moves are to occur.
-MOUNT_POINT = r"E:\XDrive\RemoteServers"
+# MOUNT_POINT = r"E:\XDrive\RemoteServers"
 # Testing:
-# MOUNT_POINT = "test/servers"
+MOUNT_POINT = "test/servers"
