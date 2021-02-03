246dabff0dea64c0b1c3b9c26d5aded6d87c41f5
Regan Sarwas <Regan_Sarwas@nps.gov>
2021-02-03 09:31:42 -0900

    lint the two rrecently fixed files

--- ifsar_scripts/list_images.py
+++ ifsar_scripts/list_images.py
@@ -12,6 +12,8 @@ import csv23
 
 
 def check_folder(folder, ext, outputter=None):
+    """Output file name and size for files with extension ext below folder."""
+
     for current_folder, _, files in os.walk(folder):
         for filename in files:
             name, extension = os.path.splitext(filename)
@@ -29,6 +31,8 @@ def check_folder(folder, ext, outputter=None):
 
 
 def main(folder, ext="tif", csv_path=None):
+    """Create an outputter (CSV of print) and output files below folder."""
+
     if csv_path is None:
         print(",".join(["folder", "filename", "ext", "size"]))
         check_folder(folder, ext)
@@ -49,14 +53,17 @@ if __name__ == "__main__":
     # SPOT5
     # main('X:\\SDMI\\SPOT5', '.tif', 'data/spot_images_x.csv')  # Subset of Z:\Spot5
     # main('Z:\\SPOT5', '.tif', 'data/spot_images_z.csv')
-    # main('Y:\\Extras\\AKR\\Statewide\\Imagery\\SDMI_SPOT5', 'tif', 'data/spot_images_y.csv')  # Duplicate of Z:\SPOT5
+    # main('Y:\\Extras\\AKR\\Statewide\\Imagery\\SDMI_SPOT5', 'tif',
+    #      'data/spot_images_y.csv')  # Duplicate of Z:\SPOT5
     # IFSAR
     # main('X:\\SDMI\\IFSAR', '.tif') #, 'data/ifsar_images_x.csv')  # Subset of Z:\IFSAR
     # main('Z:\\IFSAR', '.tif', 'data/ifsar_images_z.csv')
-    # main('Y:\\Extras\\AKR\\Statewide\\DEM\\SDMI_IFSAR', '.tif', 'data/spot_images_y.csv')  # Duplicate of Z:\IFSAR
+    # main('Y:\\Extras\\AKR\\Statewide\\DEM\\SDMI_IFSAR', '.tif',
+    #      'data/spot_images_y.csv')  # Duplicate of Z:\IFSAR
     # IKONOS
     # main(r'X:\IKONOS\LACL\BGRN', '.tif')
-    # main(r'X:\Extras\AKR\Statewide\DEM\SDMI_IFSAR', '.bil', 'c:/Users/RESarwas/Documents/GitHub/pds-reorg/data/ifsar_bil_new_x.csv')
+    # main(r'X:\Extras\AKR\Statewide\DEM\SDMI_IFSAR', '.bil',
+    #      'c:/Users/RESarwas/Documents/GitHub/pds-reorg/data/ifsar_bil_new_x.csv')
     main(
         r"X:\Extras\AKR\Statewide\DEM\SDMI_IFSAR\2021_IFSAR",
         ".tif",
--- misc-reorg-scripts/fix_tml.py
+++ misc-reorg-scripts/fix_tml.py
@@ -12,25 +12,31 @@ import fix_file
 
 def read(path):
     """Read the file as one giant string."""
-    with open(path, "r", encoding="utf-8") as fh:
-        return fh.read()
+
+    with open(path, "r", encoding="utf-8") as in_file:
+        return in_file.read()
 
 
 def save(data, path):
     """data is the entire contents of the file as one big string."""
-    with open(path, "w", encoding="utf-8") as fh:
-        fh.write(data)
+
+    with open(path, "w", encoding="utf-8") as out_file:
+        out_file.write(data)
 
 
 def read_paths(path):
+    """Return a list of paths found in the file at path."""
+
     old_paths = []
-    with open(path, "r", encoding="utf-8") as fh:
-        for line in fh:
+    with open(path, "r", encoding="utf-8") as in_file:
+        for line in in_file:
             old_paths.append(line.strip())
     return old_paths
 
 
 def build_file_mapping(in_path, all_paths):
+    """Matches old paths (in file in_path) to new paths, in all_paths."""
+
     old_paths = read_paths(in_path)
     moves = fix_file.read_csv_map(all_paths)
     results = []
@@ -43,6 +49,8 @@ def build_file_mapping(in_path, all_paths):
 
 
 def main():
+    """Read a theme list file, and moves file and create a fixed theme list."""
+
     tml = read(
         r"c:\tmp\xxx\AKR Theme List.tml"
     )  # r"X:\GIS\ThemeMgr\AKR Theme List.tml")
