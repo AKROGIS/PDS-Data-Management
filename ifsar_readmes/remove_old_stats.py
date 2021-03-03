95522570211e1f4ed3920f1e4c99137a629f129a
Regan Sarwas <regan_sarwas@nps.gov>
2021-03-03 11:47:49 -0900

    resolve pylint issues

--- ifsar_readmes/remove_old_stats.py
+++ ifsar_readmes/remove_old_stats.py
@@ -8,8 +8,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from io import open
 import os
 
-root_dir = r"X:\Extras\AKR\Statewide\DEM\SDMI_IFSAR"
+ROOT_DIR = r"X:\Extras\AKR\Statewide\DEM\SDMI_IFSAR"
 for line in open("old_style_stats.txt", "r", encoding="utf-8"):
     filename = line.strip()
-    filepath = os.path.join(root_dir, filename)
+    filepath = os.path.join(ROOT_DIR, filename)
     os.remove(filepath)
--- ifsar_scripts/build_mosaics.py
+++ ifsar_scripts/build_mosaics.py
@@ -1,35 +1,44 @@
 # -*- coding: utf-8 -*-
 """
-Add a list of rasters to a raster mosaic data set
+Extracts a list of raster image paths form a CSV file and adds to a mosaic dataset.
 """
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import csv
-import logging
 import os
-import time
 
 import arcpy
 
 import csv23
 
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.NullHandler())
 
+class Config(object):
+    """Namespace for configuration parameters. Edit as needed."""
 
-def check_arcgis_ver():
-    # ArcGIS version can be critical, check with user, prompt to continue
-    print(
-        "Mosaic datasets are not backwards compatible, ensure users are at version {0} or higher.".format(
-            arcpy.GetInstallInfo()["Version"]
-        )
-    )
-    raw_input("Press Enter to continue...")
-    logger.info("Running ArcGIS version %s" % (arcpy.GetInstallInfo()["Version"]))
+    # pylint: disable=useless-object-inheritance,too-few-public-methods
+
+    # The geodatabase of raster mosaics. This must be a local copy of the
+    # master geodatabase on the PDS.
+    mosaic_database = "C:/tmp/pds/new_ifsar/SDMI_IFSAR.gdb"
+
+    # The matching of mosaic rasters to the CSV file of paths to add.
+    # The CSV path is relative to the current working directory.
+    # The CSV has no header, and only one column which is an absolute path
+    # to a raster data file.
+    mosaic_csv = {
+        "DSM": "data/build_ifsar_2021_dsm.csv",
+        "DTM": "data/build_ifsar_2021_dtm.csv",
+        "ORI": "data/build_ifsar_2021_ori.csv",
+        "ORI_SUP1": "data/build_ifsar_2021_ori1.csv",
+        "ORI_SUP2": "data/build_ifsar_2021_ori2.csv",
+        "ORI_SUP3": "data/build_ifsar_2021_ori3.csv",
+    }
 
 
 def load_csv_file(csv_path):
+    """Return a list of the rows in the CSV."""
+
     records = []
     with csv23.open(csv_path, "r") as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -40,22 +49,21 @@ def load_csv_file(csv_path):
     return records
 
 
-def make_raster_list_for_mosaic(fgdb, mosaic, csv_data):
-    gdb_index = 0
-    mosaic_index = 1
+def make_raster_list_for_mosaic(csv_data):
+    """Extract the names of the rasters for the CSV data."""
+
     raster_path_index = 0
     rasters = []
     for record in csv_data:
-        # if record[gdb_index] == fgdb and record[gdb_index] == fgdb
         if record[raster_path_index] is not None:
             rasters.append(record[raster_path_index])
     return rasters
 
 
 def add_rasters_to_mosaic(fgdb, mosaic, rasters):
+    """Add the list of rasters to the mosaic dataset in the fgdb."""
+
     dataset = os.path.join(fgdb, mosaic)
-    # print(dataset)
-    # print(rasters)
     arcpy.AddRastersToMosaicDataset_management(
         in_mosaic_dataset=dataset,
         raster_type="Raster Dataset",
@@ -64,51 +72,16 @@ def add_rasters_to_mosaic(fgdb, mosaic, rasters):
     )
 
 
-def main(fgdb, mosaic, csv_file):
+def add_rasters():
+    """Add the rasters in csv_file to the mosaic dataset in the fgdb."""
 
-    logger.info("########################################################")
-    logger.info("Run %s %s", time.strftime("%H:%M:%S"), time.strftime("%d/%m/%Y"))
-
-    # check_arcgis_ver()
-
-    csv_data = load_csv_file(csv_file)
-    rasters = make_raster_list_for_mosaic(fgdb, mosaic, csv_data)
-    # logger.debug(rasters)
-    logger.info(
-        "Add %s rasters to %s at %s", len(rasters), mosaic, time.strftime("%H:%M:%S")
-    )
-    add_rasters_to_mosaic(fgdb, mosaic, rasters)
-    logger.info("Ended %s %s", time.strftime("%H:%M:%S"), time.strftime("%d/%m/%Y"))
-    logger.info("########################################################")
-
-
-def main2():
-    data = [
-        [r"data\SDMI_SPOT5.gdb", "SPOT5_CIR", r"data\build_spot_cir.csv"],
-        [r"data\SDMI_SPOT5.gdb", "SPOT5_RGB", r"data\build_spot_rgb.csv"],
-        [r"data\SDMI_SPOT5.gdb", "SPOT5_PAN", r"data\build_spot_pan.csv"],
-        [r"data\SDMI_SPOT5.gdb", "DEM_Ellipsoidal", r"data\build_spot_ellipsoidal.csv"],
-        [r"data\SDMI_SPOT5.gdb", "DEM_Orthometric", r"data\build_spot_orthometric.csv"],
-        [r"data\ORI_IFSAR.gdb", "ORI", r"data\build_ifsar_ori.csv"],
-        [r"data\ORI_IFSAR.gdb", "ORI_SUP", r"data\build_ifsar_ori_sup.csv"],
-        [r"data\DEM_IFSAR.gdb", "DSM", r"data\build_ifsar_dsm.csv"],
-        [r"data\DEM_IFSAR.gdb", "DTM", r"data\build_ifsar_dtm.csv"],
-    ]
-    for gdb, mos, csv_path in data:
-        main(fgdb=gdb, mosaic=mos, csv_file=csv_path)
+    fgdb = Config.mosaic_database
+    for mosaic, csv_path in Config.mosaic_csv.items():
+        csv_data = load_csv_file(csv_path)
+        rasters = make_raster_list_for_mosaic(csv_data)
+        print("Adding {0} rasters to {1}".format(len(rasters), mosaic))
+        add_rasters_to_mosaic(fgdb, mosaic, rasters)
 
 
 if __name__ == "__main__":
-    logger.addHandler(logging.StreamHandler())
-    logger.addHandler(logging.FileHandler(r"data\build-mosaics.log"))
-    logger.setLevel(logging.DEBUG)
-
-    # main2()
-    # gdb = r'X:\Mosaics\Statewide\DEMs\SDMI_IFSAR.gdb'
-    gdb = r"C:/tmp/pds/new_ifsar/SDMI_IFSAR.gdb"
-    main(fgdb=gdb, mosaic="DSM", csv_file=r"data\build_ifsar_2021_dsm.csv")
-    main(fgdb=gdb, mosaic="DTM", csv_file=r"data\build_ifsar_2021_dtm.csv")
-    main(fgdb=gdb, mosaic="ORI", csv_file=r"data\build_ifsar_2021_ori.csv")
-    main(fgdb=gdb, mosaic="ORI_SUP1", csv_file=r"data\build_ifsar_2021_ori1.csv")
-    main(fgdb=gdb, mosaic="ORI_SUP2", csv_file=r"data\build_ifsar_2021_ori2.csv")
-    main(fgdb=gdb, mosaic="ORI_SUP3", csv_file=r"data\build_ifsar_2021_ori3.csv")
+    add_rasters()
