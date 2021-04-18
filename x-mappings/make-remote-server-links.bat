bab91eaa6b740bd5ca4d60d602e6d1349c8c4773
Regan Sarwas <regan_sarwas@nps.gov>
2021-04-17 20:27:04 -0800

    Add usage notes to the script

--- x-mappings/make-remote-server-links.bat
+++ x-mappings/make-remote-server-links.bat
@@ -1,6 +1,15 @@
 @echo off
-REM Creates symbolic links for remote servers
-REM Need to run with administrative privileges
+REM Creates symbolic links to remote servers for the robo copy process
+REM Needs to be run with administrative privileges
+REM This file is typically installed in E:\XDrive\RemoteServers
+REM   on the server running the robo copy process.
+REM If a Park file server or share name changes, then:
+REM   1) Make the changes in this file and commit to the repo
+REM   2) Copy the new file to the robo copy server
+REM   3) Open up an administrative PowerShell or DOS CMD prompt
+REM   4) Delete the old junction e.g. rmdir E:\XDrive\RemoteServers\XDrive-LACL
+REM   5) Run this script e.g. E:\XDrive\RemoteServers\make-remote-server-links.bat
+REM
 mklink /D XDrive-KEFJ \\inpkefjvfs\GISData
 mklink /D XDrive-YUGA \\inpyugavmgis\GISData
 mklink /D XDrive-DENA \\inpdenagis\GIS_Data
