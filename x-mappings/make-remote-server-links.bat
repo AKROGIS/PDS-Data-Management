6d2690fd9be9521737f105308e2513d4c9a4bf92
kshakarjian <37874579+kshakarjian@users.noreply.github.com>
2021-04-19 10:53:07 -0800

    LACL KOTZ server name change

--- x-mappings/make-remote-server-links.bat
+++ x-mappings/make-remote-server-links.bat
@@ -14,12 +14,12 @@ mklink /D XDrive-KEFJ \\inpkefjvfs\GISData
 mklink /D XDrive-YUGA \\inpyugavmgis\GISData
 mklink /D XDrive-DENA \\inpdenagis\GIS_Data
 mklink /D XDrive-WRST \\INPWRSTvFS01\GIS_Data
-mklink /D XDrive-LACL \\INPLACLPAVFS\GIS
+mklink /D XDrive-LACL \\INPLACLvFS\GIS
 mklink /D XDrive-SEAN \\INPGLBAFS03\GISData
 mklink /D XDrive-KATM \\inpkatmvfs\GIS
 mklink /D XDrive-SITK \\INPSITKvFS\SITK\GISData
 mklink /D XDrive-NOME \\INPWEARNOMEvFS\GIS
-mklink /D XDrive-KOTZ \\INPWEARKOTZvFS\GIS
+mklink /D XDrive-KOTZ \\INPWEARvFS\GIS
 mklink /D XDrive-GLBA \\INPGLBAFSPri\GIS
 mklink /D XDrive-KENN \\inpwrstkennms\gis_data
 mklink /D XDrive-KLGO \\inpklgogis01\akro_gis
